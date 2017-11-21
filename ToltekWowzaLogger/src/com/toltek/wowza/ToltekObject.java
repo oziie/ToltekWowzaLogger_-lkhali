2894a5f27183d9bd5a17448db59184753f46b5aa
oziie <bilozan59@gmail.com>
2017-11-22 00:39:38 +0300

    Update ToltekObject.java
    Açıklama satırları düzenlendi.

--- ToltekWowzaLogger/src/com/toltek/wowza/ToltekObject.java
+++ ToltekWowzaLogger/src/com/toltek/wowza/ToltekObject.java
@@ -11,53 +11,49 @@ import sun.util.calendar.BaseCalendar.Date;
 
 public class ToltekObject 
 {
+	//Alan değişkenleri tanımlanıyor.
+	
 	public String app_version="0.1";
 	private String AppName;
 	private String StreamName;
 	
+	
+	// İstenirse kullanılabilinecek diğer değişkenler.
+	//Buna göre ToltekWowzaLogger içerisinde gerekli methodlar da düzenlenmeli.
 	
 	//private Date date;
 	//private IApplicationInstance AppName;
 	//private IMediaStream stream;
 	
 	
-
+        //Yapıcı methodlar ToltekWowzaLogger daki methodlardan gelen nesne türlerine göre
+	//parametrelere ayrılmış olarak oluşturuluyor.
+	
+	//IApplicationInstance nesnesi alır.
 	public ToltekObject(IApplicationInstance appInstance)
-	{
-		setAppName(appInstance);
-		
-	}
+	{ setAppName(appInstance); } 
 	
+	//IMediaStream nesnesi alır.
 	public ToltekObject(IMediaStream stream)
-	{
-		setStreamName(stream);
-		
-	}
+	{ setStreamName(stream); }
 	
+		//AppName için getApplication().getName() Wowza'nın isim getirmek için
+		//yazdığı metodlardır.
 		private void setAppName(IApplicationInstance appInstance) 
-	
+			
 		{ AppName=appInstance.getApplication().getName(); }
 	
 		public String getAppName() 
 	
 		{  return AppName;  }
 
+		//getName() Wowza tarafından yazılmış bir metodtur.
 		private void setStreamName(IMediaStream stream) 
 		
 		{ StreamName=stream.getName(); }
 	
-	
 		public String getStreamName() 
 		
 		{  return StreamName;  }
-
-		
 	
-
-	
-	
-	
-
-	
-
 }
